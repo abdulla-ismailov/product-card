c21259fe44e358d483c52a1a36a22ea67eb14e7d
abdulla-ismailov <abdula.ismailov221@gmail.com>
2026-08-19 13:14:36 +0300

    подправил названия переменных и табуляцию

--- homework-8.js
+++ homework-8.js
@@ -2,11 +2,11 @@
 
 
 const man = {
-    name:"Абдулла",
-    family:"Исмаилов",
-    Age:17,
-    city:"Махачкала",
-    work:"Нету",
+    name: "Абдулла",
+    family: "Исмаилов",
+    Age: 17,
+    city: "Махачкала",
+    work: "Нету",
 }
 
 console.log(man)
@@ -16,38 +16,35 @@ console.log(man)
 
 
 const car = {
-    brand:"BMW",
-    model:"M3",
-    yearManufacture:"2021",
-    color:"red",
-    typeBox:"automatic",
+    brand: "BMW",
+    model: "M3",
+    yearManufacture: "2021",
+    color: "red",
+    typeBox: "automatic",
 }
 
-    console.log(car)
+console.log(car)
 
 
 //владелец авто
 
 
-const owner = {
-    ownerAuto:"Андрей",
-}
+const owner = man;
 
-    console.log(owner)
+console.log(owner)
 
 
 //функция про макс скорость
 
 
-function checkMaxSpeed (showMaxSpeed) {
+function checkMaxSpeed(showMaxSpeed) {
     if (!showMaxSpeed.hasOwnProperty("maxSpeed")) {
         showMaxSpeed["maxSpeed"] = 302;
         console.log(`Свойство "maxSpeed" добавлено. Значение: ${showMaxSpeed["maxSpeed"]}`);
-    }
-    else{
+    } else {
         console.log("Свойство 'maxSpeed' уже существует. Ничего не делаем.");
     }
-    return showMaxSpeed
+    return showMaxSpeed;
 }
 
 checkMaxSpeed(car);
@@ -57,16 +54,15 @@ checkMaxSpeed(car);
 //функция, получает первым аргументом — объект, вторым аргументом — свойство объекта, которое нужно вывести.
 
 
-function getInfo (object, property) {
+function showInfo(object, property) {
     if (object && object.hasOwnProperty(property)) {
         console.log(object[property]);
-    }
-    else {
+    } else {
         console.log(`свойство "${property}" не найдено`);
     }
 }
 
-getInfo(man, `surname`);
+showInfo(man, `surname`);
 
 
 //массив 
@@ -80,45 +76,47 @@ console.log(colors)
 //массив с 4-5 книгами
 
 
-const books = [{
+const books = [
+    {
     title: '1984',
     author: 'Джордж Оруэлл',
     yearPublication: 1949,
-    coverColor:'красный',
-    genre:'антиутопия',
-},
+    coverColor: 'красный',
+    genre: 'антиутопия',
+    },
 
-{
+    {
     title: 'Гарри Поттер и философский камень',
     author: 'Дж. К. Роулинг',
     yearPublication: 1997,
-    coverColor:'Синий',
-    genre:'Фэнтези',
-},
+    coverColor: 'Синий',
+    genre: 'Фэнтези',
+    },
 
-{
+    {
     title: 'Маленький принц',
     author: 'Антуан де Сент-Экзюпери',
     yearPublication: 1943,
-    coverColor:'Жёлтый',
-    genre:'Сказка',
-},
+    coverColor: 'Жёлтый',
+    genre: 'Сказка',
+    },
 
-{
+    {
     title: 'Преступление и наказание',
     author: 'Фёдор Достоевский',
     yearPublication: 1866,
-    coverColor:'Коричневый',
-    genre:'Роман',
-},
+    coverColor: 'Коричневый',
+    genre: 'Роман',
+    },
 
-{
+    {
     title: 'Властелин колец',
     author: 'Дж. Р. Р. Толкин',
     yearPublication: 1954,
-    coverColor:'Зелёный',
-    genre:'Фэнтези',
-},];
+    coverColor: 'Зелёный',
+    genre: 'Фэнтези',
+    },
+];
 
 
 //добавляем книгу в конец
@@ -128,8 +126,8 @@ books.push({
     title: 'Шерлок Холмс: Собака Баскервилей',
     author: 'Артур Конан Дойл',
     yearPublication: 1902,
-    coverColor:'Чёрный',
-    genre:'Детектив',
+    coverColor: 'Чёрный',
+    genre: 'Детектив',
 });
 
 console.log(books)
@@ -142,45 +140,48 @@ const booksHarryPotter = [{
     title: 'Гарри Поттер и философский камень',
     author: 'Дж. К. Роулинг',
     yearPublication: 1997,
-    coverColor:'Красный',
-    genre:'Фэнтези',
-},
+    coverColor: 'Красный',
+    genre: 'Фэнтези',
+    },
 
-{
+    {
     title: 'Гарри Поттер и Тайная комната',
     author: 'Дж. К. Роулинг',
     yearPublication: 1998,
-    coverColor:'Зелёный',
-    genre:'Фэнтези',
-},
+    coverColor: 'Зелёный',
+    genre: 'Фэнтези',
+    },
 
-{
+    {
     title: 'Гарри Поттер и узник Азкабана',
     author: 'Дж. К. Роулинг',
     yearPublication: 1999,
-    coverColor:'Синий',
-    genre:'Фэнтези',
-},];
+    coverColor: 'Синий',
+    genre: 'Фэнтези',
+    },
+];
 
 
 //метод спред
 
 
-const allBooks = [...books, ...booksHarryPotter]
-console.log(allBooks)
+const allBooks = [...books, ...booksHarryPotter];
+console.log(allBooks);
 
 
 //редкость книг
 
 
-function showRareBooks(arrayBooks) {
-    return arrayBooks.map(book=> {
-        const newBook = {...book};
+function getRareBooks(arrayBooks) {
+    return arrayBooks.map((book) => {
+        const newBook = { ...book };
+        
         newBook.isRare = book.yearPublication > 2000;
+        
         return newBook;
-    })
+    });
 }
 
-const rareBooks = showRareBooks(allBooks);
+const rareBooks = getRareBooks(allBooks);
 
 console.log(rareBooks)
